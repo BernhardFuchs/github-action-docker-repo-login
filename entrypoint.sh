a7db5bbaf07fa5a09f47021c4c932c0874ac1e41
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2020-09-24 11:53:01 +0200

    feat: separate env variables for dockerhub and private repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [ "$INPUT_REPO_NAME" == "Dockerhub" ]; then
-    docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+    docker login -u "${DOCKERHUB_USERNAME}" -p "${DOCKERHUB_PASSWORD}"
 else
-    docker login "$INPUT_REPO_NAME" -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+    docker login "$INPUT_REPO_NAME" -u "${DOCKER_REPO_USERNAME}" -p "${DOCKER_REPO_PASSWORD}"
 fi
