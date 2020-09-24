6a38645eaa7f82535c657d96a3bdd435d78550a5
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2020-09-24 12:45:10 +0200

    feat: use password-stdin for login

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
 if [ "$INPUT_REPO_NAME" == "Dockerhub" ]; then
-    docker login -u "${DOCKERHUB_USERNAME}" -p "${DOCKERHUB_PASSWORD}"
+    echo "Login to Dockerhub"
+    echo "${DOCKER_REPO_PASSWORD}" | docker login -u "${DOCKERHUB_USERNAME}" --password-stdin
 else
-    docker login "$INPUT_REPO_NAME" -u "${DOCKER_REPO_USERNAME}" -p "${DOCKER_REPO_PASSWORD}"
+    echo "Login to $INPUT_REPO_NAME"
+    echo "${DOCKER_REPO_PASSWORD}" | docker login "$INPUT_REPO_NAME" -u "${DOCKER_REPO_USERNAME}" --password-stdin
 fi
